3b09873f62489f3ff5f374faaeac584b26e8789c
MadJangE00 <jiniixz@naver.com>
2021-02-01 19:55:21 +0900

    build plan

---
+++ main.py
@@ -0,0 +1,18 @@
+# start(click button)
+# time running (300 - t)
+# score ( 0 + num of creatures /sec)
+
+# create object as click
+# kinds of creatures
+# Queen, Jack, Normal
+# objects life (birth and death) Event
+
+# End(time over)
+
+
+
+
+
+
+
+
